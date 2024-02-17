2c1b928b10cb25812993cbd1a5f21bca813dbc37
omar alshobky <93231975za@gmail.com>
2024-02-17 11:40:56 +0200

    one commit

---
+++ app/src/main/java/com/alshobky/tictactoc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alshobky.tictactoc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
