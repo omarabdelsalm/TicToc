7750ae935ba0539816d3c104b48b2c4ebb9f4a68
omar alshobky <93231975za@gmail.com>
2024-02-24 22:00:23 +0200

    New Change and import add

--- app/src/main/java/com/alshobky/tictactoc/MainActivity.kt
+++ app/src/main/java/com/alshobky/tictactoc/MainActivity.kt
@@ -4,42 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
-import java.util.Random
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bu1:Button
-    lateinit var bu2:Button
-    lateinit var bu3:Button
-    lateinit var bu4:Button
-    lateinit var bu5:Button
-    lateinit var bu6:Button
-    lateinit var bu7:Button
-    lateinit var bu8:Button
-    lateinit var bu9:Button
-    lateinit var txt1:TextView
-    lateinit var txt2:TextView
-    lateinit var bu:Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bu1=findViewById(R.id.bu1)
-        bu2=findViewById(R.id.bu2)
-        bu3=findViewById(R.id.bu3)
-        bu4=findViewById(R.id.bu4)
-        bu5=findViewById(R.id.bu5)
-        bu6=findViewById(R.id.bu6)
-        bu7=findViewById(R.id.bu7)
-        bu8=findViewById(R.id.bu8)
-        bu9=findViewById(R.id.bu9)
-        bu=findViewById(R.id.btnRest)
-        txt1=findViewById(R.id.txtView1)
-        txt2=findViewById(R.id.txtView2)
-        bu.setOnClickListener {
-            restartGame()
-        }
     }
     fun butClik(view :View){
         val buSelected=view as Button
@@ -55,39 +25,35 @@ class MainActivity : AppCompatActivity() {
             R.id.bu8->butId=8
             R.id.bu9->butId=9
         }
-        // Log.d("select :",buSelected.id.toString())
-        // Log.d("select :",butId.toString())
+       // Log.d("select :",buSelected.id.toString())
+       // Log.d("select :",butId.toString())
         playGame(butId,buSelected)
     }
     //new mothed to play game
-     var activePlayer=1
-     var player1=ArrayList<Int>()
-     var player2=ArrayList<Int>()
-    fun playGame(butId:Int,buSelected:Button){
-        if(activePlayer==1){
-            buSelected.text="X"
-            buSelected.setBackgroundResource(R.color.butRed)
-            player1.add(butId)
-            activePlayer=2
-            autoPlay()
-        }else{
-            buSelected.text="o"
-            buSelected.setBackgroundResource(R.color.greenTack)
-            player2.add(butId)
-            activePlayer=1
-
+        private var activePlayer=1
+        private var player1=ArrayList<Int>()
+        private var player2=ArrayList<Int>()
+        fun playGame(butId:Int,buSelected:Button){
+          if(activePlayer==1){
+              buSelected.text="X"
+              buSelected.setBackgroundResource(R.color.butRed)
+              player1.add(butId)
+              activePlayer=2
+          }else{
+              buSelected.text="O"
+              buSelected.setBackgroundResource(R.color.greenTack)
+              player2.add(butId)
+              activePlayer=1
+          }
+           buSelected.isEnabled=false
+            chickWiner()
         }
-        buSelected.isEnabled=false
-        chickWiner()
-    }
-
-    // fun for chick winer
+   var winer=-1
     fun  chickWiner(){
-        var winer=-1
         //row 1
-        if(player1.contains(1) && player1.contains(2) && player1.contains(3)) {
-            winer=1
-        }
+       if(player1.contains(1) && player1.contains(2) && player1.contains(3)) {
+           winer=1
+       }
         if(player2.contains(1) && player1.contains(2) && player1.contains(3)) {
             winer=2
         }
@@ -127,79 +93,14 @@ class MainActivity : AppCompatActivity() {
             winer=2
         }
         if(winer==1){
-            player1Couunts+=1
             Toast.makeText(this,"the winer is player 1",Toast.LENGTH_LONG).show()
-            restartGame()
-            txt1.text=player1Couunts.toString()
         }else if(winer==2){
-            player2Couunts+=1
             Toast.makeText(this,"the winer is player 2",Toast.LENGTH_LONG).show()
-            restartGame()
-            txt2.text=player2Couunts.toString()
         }
 
     }
 
-    // fun for autoplay and select cell
-    fun autoPlay() = try {
-        var emptyCells=ArrayList<Int>()
-        for(cellId in 1..9){
-            if(!(player1.contains(cellId) || player2.contains(cellId))){
-                emptyCells.add(cellId)
-            }
-        }
 
-
-        var r= Random()
-        var randIndex=r.nextInt(emptyCells.size)
-        val cellId=emptyCells[randIndex]
-
-        val buSelected:Button? = when(cellId){
-            1->bu1
-            2->bu2
-            3->bu3
-            4->bu4
-            5->bu5
-            6->bu6
-            7->bu7
-            8->bu8
-            9->bu9
-
-            else -> {bu1}
-        }
-        playGame(cellId,buSelected!!)
-
-    }catch(e:Exception){
-        restartGame()
-    }
-    // create fun to reset game on
-    // rest count for winer
-    var player1Couunts=0
-    var player2Couunts=0
-    fun restartGame(){
-        activePlayer=1
-        player1.clear()
-        player2.clear()
-        for(cellId in 1..9){
-            val buSelected:Button? = when(cellId){
-                1->bu1
-                2->bu2
-                3->bu3
-                4->bu4
-                5->bu5
-                6->bu6
-                7->bu7
-                8->bu8
-                9->bu9
-
-                else -> {bu1}
-            }
-            buSelected!!.text=""
-            buSelected.setBackgroundResource(R.color.butWhit)
-            buSelected.isEnabled=true
-
-        }
-    }
 
 
 
